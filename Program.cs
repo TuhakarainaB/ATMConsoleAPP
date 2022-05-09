20a5fbc03defe53d856187ee0b3039a6af424dcb
TuhakarainaB <Bruce.Tuhakaraina@toiohomai.ac.nz>
2022-05-10 10:12:27 +1200

    Completed CheckInput start of ProcessMenu

--- Program.cs
+++ Program.cs
@@ -16,6 +16,9 @@ namespace ATMConsoleAPP
         // User Balance of funds
         public static decimal balance;
 
+        // menu option that user has selected
+        public static string userOption = "";
+
         private static void CheckPin()
         {
             string pin = "";
@@ -52,8 +55,75 @@ namespace ATMConsoleAPP
             Console.WriteLine("\t [3] Deposit Funds");
             Console.WriteLine("\t [4] Exit Application\n");
             Console.WriteLine("****************************************\n");
+
+            // run checkinput
+            CheckInput();
         }
-        
+
+        public static void CheckInput()
+        {
+            bool check = false;
+
+            while (check == false)
+            {
+                Console.WriteLine("Please choose an option ([1], [2], [3], [4])");
+                string userInput = Console.ReadLine();
+
+                // loop thru each item in the array (menuOptions) save each interation in the temp variable
+                foreach (string temp in menuOptions)
+                {
+                    // check if users input equals what is in our array (now saved as temp variable)
+                    // return true or return false save result into check.
+                    check = userInput.Equals(temp);
+
+                    // break out of the foreach loop if check = true
+                    if (check)
+                    {
+                        break;
+                    }
+
+                } // end of foreach
+
+                if (check)
+                {
+                    Console.WriteLine("Your input " + userInput + " is valid");
+                }
+                else
+                {
+                    Console.WriteLine("Your input " + userInput + " is not valid");
+                }
+
+                userOption = userInput;
+                ProcessMenu();
+                
+
+            } // end of while loop
+
+        } // end of CheckInput
+
+        static void ProcessMenu()
+        {
+            if(userOption == "1")
+            {
+                Console.WriteLine("\t [1] View Balance");
+                MainMenu();
+            }
+            if (userOption == "2")
+            {
+                Console.WriteLine("\t [2] Withdraw Funds");
+                MainMenu();
+            }
+            if (userOption == "3")
+            {
+                Console.WriteLine("\t [3] Deposit Funds");
+                MainMenu();
+            }
+            if (userOption == "4")
+            {
+                Console.WriteLine("\t [4] Exit Application");
+            }
+        } // end ProcessMenu
+
         static void Main(string[] args)
         {
             CheckPin();
